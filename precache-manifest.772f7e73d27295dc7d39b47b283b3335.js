6cd2a2b59ac3d9e8d304dec6fc4e2f400826dc2f
prince kumar <princekumarcena@gmail.com>
2019-12-19 12:56:56 +0530

    Updates

---
+++ precache-manifest.772f7e73d27295dc7d39b47b283b3335.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10daf70494932de95b35570c92cce1d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1184c20f85adb1658bc8",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "feb2f76f04d05bc5e91a",
+    "url": "/static/css/main.2cf71fb0.chunk.css"
+  },
+  {
+    "revision": "1184c20f85adb1658bc8",
+    "url": "/static/js/2.c999fc29.chunk.js"
+  },
+  {
+    "revision": "67935b6d863b3e23bc08cf31dbc40ecc",
+    "url": "/static/js/2.c999fc29.chunk.js.LICENSE"
+  },
+  {
+    "revision": "feb2f76f04d05bc5e91a",
+    "url": "/static/js/main.26b551e4.chunk.js"
+  },
+  {
+    "revision": "aa6fed8716c2be098a58",
+    "url": "/static/js/runtime-main.55a556ca.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
